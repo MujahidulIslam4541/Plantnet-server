04deeadee1c44f0c35e88747f12b1caa22691c95
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-04-09 20:50:49 +0600

    some changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000
 const app = express()
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'https://plantnet-project-285c8.web.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
@@ -489,10 +489,10 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+    // await client.db('admin').command({ ping: 1 })
+    // console.log(
+    //   'Pinged your deployment. You successfully connected to MongoDB!'
+    // )
   } finally {
     // Ensures that the client will close when you finish/error
   }
